2d75f7470359eedf62ee357a0d4fb3cb5654555f
Joseph Romeo <jromeo1982@gmail.com>
2017-07-17 22:03:24 -0600

    Moved client code into client directory

--- tracker/server.js
+++ tracker/server.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var path = require('path');
+//var path = require('path');
 path = __dirname + '/';
 
 app.use(express.static(path + 'node_modules'));
-app.use(express.static(path + 'data_visualizer'));
-app.use(express.static(path + 'css'));
-app.use(express.static(path + 'bootstrap'));
+app.use(express.static(path + 'client'));
 app.use(express.static(path));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path + 'tracker.html');
+    res.sendFile(path + 'client/tracker.html');
 });
 
 app.listen(3000, "0.0.0.0");
