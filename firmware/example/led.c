747e8e654f86e73ed8c955054264d271ca5bc459
Joseph Romeo <jromeo1982@gmail.com>
2017-08-10 20:57:46 -0600

    Updated LED blink for atmega 2560

--- firmware/example/led.c
+++ firmware/example/led.c
@@ -6,15 +6,15 @@
 int main (void)
 {
     /* set pin 5 of PORTB for output*/
-    DDRB |= _BV(DDB5);
+    DDRB |= _BV(DDB7);
 
 		while(1) {
 				/* set pin 5 high to turn led on */
-				PORTB |= _BV(PORTB5);
+				PORTB |= _BV(PORTB7);
 				_delay_ms(BLINK_DELAY_MS);
 				
 				/* set pin 5 low to turn led off */
-				PORTB &= ~_BV(PORTB5);
+				PORTB &= ~_BV(PORTB7);
 				_delay_ms(BLINK_DELAY_MS);
 		}
 }
