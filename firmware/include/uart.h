41c8ac5eb70ef0985a19d013f6a30261e1fdf625
Joseph Romeo <jromeo1982@gmail.com>
2017-08-11 00:49:05 -0600

    Split uart access into separate file

---
+++ firmware/include/uart.h
@@ -0,0 +1,7 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+// initializing serial 0 and 1 to run at 9600bps 8 data bits, 1 stop bit, no parity
+void UartInit();
+
+#endif // __UART_H__
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,32 +1,23 @@
+
+// AVR library 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+// utility functions
 #include "utils/ringbuffer.h"
- 
-#define BAUDRATE 9600
-#define BAUDSCALE (((F_CPU / (BAUDRATE * 16UL))) - 1)
 
+// mcu hardware access
+#include "uart.h"
+ 
 
 int main (void)
 {
   // disabling interrupts globally during setup
   cli();
 
-  // initializing UART0/1 to baud rate to 9600 
-  UBRR0 = BAUDSCALE;
-  UBRR1 = BAUDSCALE;
-
-  // setting 8 data bits, 1 stop bit, no paritty
-  UCSR0C = ((0 << USBS0) | (1 << UCSZ00) | (1 << UCSZ01));
-  UCSR1C = ((0 << USBS1) | (1 << UCSZ10) | (1 << UCSZ11));
-
-  // enabling receiver and transmitter
-  UCSR0B = ((1 << RXEN0) | (1 << TXEN0));
-  UCSR1B = ((1 << RXEN1) | (1 << TXEN1));
-
-  // enabling usart interrupts
-  UCSR0B |= ((1 << RXCIE0) | (1 << TXCIE0));
+  // initializing uart
+  UartInit();
 
   // enabling interrupts globally
   sei();
@@ -36,11 +27,4 @@ int main (void)
   }
 }
 
-ISR(USART0_RX_vect)
-{
-  char rxByte;
 
-  // echo rx to tx
-  rxByte = UDR0;
-  UDR0 = rxByte;
-}
