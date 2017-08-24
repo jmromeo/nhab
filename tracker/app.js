aafad6aa560e8206394c1e1017246fb554959e46
Joseph Romeo <jromeo1982@gmail.com>
2017-08-23 23:30:00 -0600

    Renamed server.js to app.js

--- tracker/app.js
+++ tracker/app.js
@@ -8,19 +8,19 @@ path = __dirname + '/';
 
 
 app.use(express.static(path + 'node_modules'));
-//app.use(express.static(path + 'node_modules/socket.io-client/dist'));
 app.use(express.static(path + 'client'));
 app.use(express.static(path));
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:3000
 app.get('/', function(req, res) {
     res.sendFile(path + 'client/tracker.html');
 });
 
 server.listen(3000);
-//app.listen(3000, "0.0.0.0");
 
 // socket server connection callback
 io.on('connection', function (socket) {
     console.log('a user connected');
 });
+
+
